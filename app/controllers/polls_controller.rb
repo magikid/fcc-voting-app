8361e39b55e69ed25c385800f9c72feeb98ff9b9
Chris W Jones <chris@christopherjones.us>
2017-03-30 17:47:44 -0400

    Adds basic polls creation
    This adds some logic for creating a poll.  Still no options for the poll
    but unauthenticated users can see the published polls at least.

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,6 +1,6 @@
 class PollsController < ApplicationController
   before_action :set_poll, only: [:show, :edit, :update, :destroy]
-  before_action :require_login, only: [:new, :edit, :update, :destroy]
+  before_action :require_login, except: [:show]
 
   # GET /polls
   # GET /polls.json
@@ -25,30 +25,22 @@ class PollsController < ApplicationController
   # POST /polls
   # POST /polls.json
   def create
-    @poll = Poll.new(poll_params)
+    @poll = current_user.polls.new(poll_params)
 
-    respond_to do |format|
-      if @poll.save
-        format.html { redirect_to @poll, notice: 'Poll was successfully created.' }
-        format.json { render :show, status: :created, location: @poll }
-      else
-        format.html { render :new }
-        format.json { render json: @poll.errors, status: :unprocessable_entity }
-      end
+    if @poll.save
+      redirect_to @poll, notice: 'Poll was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /polls/1
   # PATCH/PUT /polls/1.json
   def update
-    respond_to do |format|
-      if @poll.update(poll_params)
-        format.html { redirect_to @poll, notice: 'Poll was successfully updated.' }
-        format.json { render :show, status: :ok, location: @poll }
-      else
-        format.html { render :edit }
-        format.json { render json: @poll.errors, status: :unprocessable_entity }
-      end
+    if @poll.update(poll_params)
+      redirect_to @poll, notice: 'Poll was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -56,10 +48,7 @@ class PollsController < ApplicationController
   # DELETE /polls/1.json
   def destroy
     @poll.destroy
-    respond_to do |format|
-      format.html { redirect_to polls_url, notice: 'Poll was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to polls_url, notice: 'Poll was successfully destroyed.'
   end
 
   private
@@ -70,6 +59,6 @@ class PollsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def poll_params
-      params.require(:poll).permit(:title, :published, :user_id)
+      params.require(:poll).permit(:title, :published)
     end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @all_polls = Poll.where(published: true).order(created_at: :desc).limit(50)
   end
 end
